5fc61fed8bbd06559b357b658c098596d74a3c59
Ayush <133644378+ayush071203@users.noreply.github.com>
2023-07-30 23:01:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+$('#vendor-service-1').on("mouseenter" , function(){
+    $("#third-text").css("color" , "white");
+})
+
+$('#vendor-service-1').on("mouseleave" , function(){
+    $("#third-text").css("color" , "rgb(95 , 56 ,158)");
+})
+
+$('#vendor-service-2').on("mouseenter" , function(){
+    $("#fourth-text").css("color" , "white");
+})
+
+$('#vendor-service-2').on("mouseleave" , function(){
+    $("#fourth-text").css("color" , "rgb(95 , 56 ,158)");
+})
+
+
+$('#vendor-service-3').on("mouseenter" , function(){
+    $("#fifth-text").css("color" , "white");
+})
+
+$('#vendor-service-3').on("mouseleave" , function(){
+    $("#fifth-text").css("color" , "rgb(95 , 56 ,158)");
+})
+
+$('#vendor-service-4').on("mouseenter" , function(){
+    $("#sixth-text").css("color" , "white");
+})
+
+$('#vendor-service-4').on("mouseleave" , function(){
+    $("#sixth-text").css("color" , "rgb(95 , 56 ,158)");
+})
+
+$('#vendor-service-5').on("mouseenter" , function(){
+    $("#seventh-text").css("color" , "white");
+})
+
+$('#vendor-service-5').on("mouseleave" , function(){
+    $("#seventh-text").css("color" , "rgb(95 , 56 ,158)");
+})
+
+$('#vendor-service-6').on("mouseenter" , function(){
+    $("#eigth-text").css("color" , "white");
+})
+
+$('#vendor-service-6').on("mouseleave" , function(){
+    $("#eigth-text").css("color" , "rgb(95 , 56 ,158)");
+})
+
+
+$("#venue-1").on("mouseenter" , function(){
+    $("#jaipur").css("color" , "white");
+})
+$("#venue-1").on("mouseleave" , function(){
+    $("#jaipur").css("color" , "rgb(95, 56, 158)");
+})
+
+$("#venue-2").on("mouseenter" , function(){
+    $("#jaipura").css("color" , "white");
+})
+$("#venue-2").on("mouseleave" , function(){
+    $("#jaipura").css("color" , "rgb(95, 56, 158)");
+})
+
+$("#venue-3").on("mouseenter" , function(){
+    $("#jaipuraa").css("color" , "white");
+})
+$("#venue-3").on("mouseleave" , function(){
+    $("#jaipuraa").css("color" , "rgb(95, 56, 158)");
+})
+
+$("#venue-4").on("mouseenter" , function(){
+    $("#jaipuraaaa").css("color" , "white");
+})
+$("#venue-4").on("mouseleave" , function(){
+    $("#jaipuraaaa").css("color" , "rgb(95, 56, 158)");
+})
+
+$("#home").click(function(){
+    $(document).scrollTop(100);
+})
+
+$("#servicessssss").click(function(){
+    $(document).scrollTop(650);
+})
+
+$("#vendoe").click(function(){
+    $(document).scrollTop(1250);
+})
+
+$("#venu").click(function(){
+    $(document).scrollTop(2200);
+})
+
+$("#con").click(function(){
+    $(document).scrollTop(2900);
+})
